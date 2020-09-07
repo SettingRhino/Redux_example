9ca4060c3c655df21d8329d6f8d0421f8163d4f2
SettingRhino <tjtj54441@gmail.com>
2020-09-07 17:52:15 +0900

    GraphDB GraphDB

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 import configureStore from "./configureStore";
-import "./styles.css";
 //import { setLoading, resetLoading } from "./actions/loadingActions";
 //import { setUser } from "./actions/userActions";
 import { setCollection, setAge } from "./actions/collectionAction";
